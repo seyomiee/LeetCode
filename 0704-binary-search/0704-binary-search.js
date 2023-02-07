3111f5040498d9af772b5061f79e61fe61ce861f
non <59796469+seyomiee@users.noreply.github.com>
2023-02-07 17:40:57 +0900

    Time: 67 ms (77.24%), Space: 44.5 MB (92.53%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+let search = function(nums, target) {
+    let start=0, end=nums.length-1;
+    while(start<=end){
+        let mid= parseInt((start+end)/2);
+        if(nums[mid]==target){return mid; }
+        else if(nums[mid]<target){
+            start=mid+1;}
+        else{
+            end=mid-1;}
+    }return -1;
+};
